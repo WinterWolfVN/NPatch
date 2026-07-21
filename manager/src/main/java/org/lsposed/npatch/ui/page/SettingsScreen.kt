0c370b20ce038c2c463886c6bc57065b5a6102dd
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-07-21 18:23:23 +0700

    Update SettingsScreen.kt

--- manager/src/main/java/org/lsposed/npatch/ui/page/SettingsScreen.kt
+++ manager/src/main/java/org/lsposed/npatch/ui/page/SettingsScreen.kt
@@ -1,7 +1,6 @@
 package org.lsposed.npatch.ui.page
 
 import android.app.Activity
-import android.app.Activity
 import android.content.Intent
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
