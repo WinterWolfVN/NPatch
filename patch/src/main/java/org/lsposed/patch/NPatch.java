31bc8a75af5e089c785d0c4de158854d4d1af86e
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-07-27 19:58:33 +0700

    Add files via upload

--- patch/src/main/java/org/lsposed/patch/NPatch.java
+++ patch/src/main/java/org/lsposed/patch/NPatch.java
@@ -538,6 +538,7 @@ public class NPatch {
           provider.put("authorities", packageName + ".ProviderStub");   
           provider.put("exported", "false");
           provider.put("initOrder", "999");
+          property.addProvider(provider);
 
         if (!targetPackage.equals(originPackage)) {
             property.addManifestAttribute(new AttributeItem(NodeValue.Manifest.PACKAGE, targetPackage).setNamespace(null));
