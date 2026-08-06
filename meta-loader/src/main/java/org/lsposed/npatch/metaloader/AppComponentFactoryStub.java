2e88c3bca3d0f2cd3b630a96cecc787d3a8dee78
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-06 20:26:21 +0700

    Update AppComponentFactoryStub.java

--- meta-loader/src/main/java/org/lsposed/npatch/metaloader/AppComponentFactoryStub.java
+++ meta-loader/src/main/java/org/lsposed/npatch/metaloader/AppComponentFactoryStub.java
@@ -1,6 +1,7 @@
 package org.lsposed.npatch.metaloader;
 
-import android.app.LoadedApk;
+import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.pm.ProviderInfo;
 import android.database.Cursor;
@@ -9,22 +10,34 @@ import android.util.Log;
 
 import java.lang.reflect.Method;
 
-public final class AppComponentFactoryStub {    
-    private static final String TAG = "NPatch-Metaloader";    
-    
-    public void run(Context context) {            
-        android.util.Log.e(TAG, "Provider has been activated");        
+public final class AppComponentFactoryStub extends ContentProvider {    
+    private static final String TAG = "NPatch-Metaloader";
+    private static volatile boolean sDone;
+
+    @Override
+    public void attachInfo(Context context, ProviderInfo info) {
+        super.attachInfo(context, info);
+        android.util.Log.e(TAG, "Provider has been activated");
+        if (sDone) return;
+        sDone = true;  
 
         try {
-            ClassLoader cl = AppComponentFactoryStub.class.getClassLoader();
             Class<?> cls = Class.forName(
                     "org.lsposed.npatch.metaloader.LSPAppComponentFactoryStub",
                     true,
                     context.getClassLoader()                    
-            );            
+            );
             android.util.Log.e(TAG, "LSPAppComponentFactoryStub has been activated");          
         } catch (Throwable t) {
             android.util.Log.e(TAG, "Bug"  + android.util.Log.getStackTraceString(t));
         }
     }
-}    
+
+    @Override public boolean onCreate() { return true; }
+    @Override public Cursor query(Uri uri, String[] p, String s, String[] a, String o) { return null; }
+    @Override public String getType(Uri uri) { return null; }
+    @Override public Uri insert(Uri uri, ContentValues v) { return null; }
+    @Override public int delete(Uri uri, String s, String[] a) { return 0; }
+    @Override public int update(Uri uri, ContentValues v, String s, String[] a) { return 0; }
+                }
+                               
