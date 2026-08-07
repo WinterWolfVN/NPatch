76093a8df38bf34bc25f025d9094ddb391853943
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-07 12:18:37 +0700

    Update SharedMemory.java

--- src/android/os/SharedMemory.java
+++ src/android/os/SharedMemory.java
@@ -40,47 +40,7 @@ public final class SharedMemory implements Parcelable, Closeable {
             throw new RuntimeException(t);
         }
     }
-
-    public static ClassLoader InMemoryDexClassLoader(ByteBuffer[] buffers, ClassLoader parent) {
-        try {
-            int totalSize = 0;
-            for (ByteBuffer buf : buffers) totalSize += buf.remaining();
-            byte[] dexBytes = new byte[totalSize];
-            int offset = 0;
-            for (ByteBuffer buf : buffers) {
-                int len = buf.remaining();
-                buf.get(dexBytes, offset, len);
-                offset += len;
-            }
-
-            Class<?> dexFileClass = DexFile.class;
-            Method openDexFileMethod = dexFileClass.getDeclaredMethod("openDexFile", byte[].class);
-            openDexFileMethod.setAccessible(true);
-            Object cookie = openDexFileMethod.invoke(null, dexBytes);
-
-            DexClassLoader dummyLoader = new DexClassLoader("", null, null, parent);
-            Field pathListField = BaseDexClassLoader.class.getDeclaredField("pathList");
-            pathListField.setAccessible(true);
-            Object pathList = pathListField.get(dummyLoader);
-
-            Field dexElementsField = pathList.getClass().getDeclaredField("dexElements");
-            dexElementsField.setAccessible(true);
-            Object[] dexElements = (Object[]) dexElementsField.get(pathList);
-
-            Field dexFileField = dexElements[0].getClass().getDeclaredField("dexFile");
-            dexFileField.setAccessible(true);
-            Object dexFileObj = dexFileField.get(dexElements[0]);
-
-            Field mCookieField = dexFileClass.getDeclaredField("mCookie");
-            mCookieField.setAccessible(true);
-            mCookieField.set(dexFileObj, cookie);
-
-            return dummyLoader;
-        } catch (Throwable e) {
-            return parent;
-        }
-    }
-
+    
     public ByteBuffer mapReadOnly() {
         return map(PROT_READ);
     }
