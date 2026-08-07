ec10a70da58635874de0a3432275d8c8ba071d78
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-07 21:23:48 +0700

    Update ModuleLoader.java

--- share/android/src/main/java/org/lsposed/npatch/util/ModuleLoader.java
+++ share/android/src/main/java/org/lsposed/npatch/util/ModuleLoader.java
@@ -1,6 +1,6 @@
 package org.lsposed.npatch.util;
 
-import android.os.SharedMemory;
+import oldlib.android.os.SharedMemory;
 import android.system.ErrnoException;
 import android.system.OsConstants;
 import android.util.Log;
