9bd638ef8cf9859b9896616ea99645a0d551802c
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-11 17:09:59 +0700

    Temporary

--- oldlib/dalvik/system/InMemoryDexClassLoader.java
+++ oldlib/dalvik/system/InMemoryDexClassLoader.java
@@ -1,53 +1,112 @@
 package oldlib.dalvik.system;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
-import dalvik.system.BaseDexClassLoader;
 import dalvik.system.DexClassLoader;
-import dalvik.system.DexFile;
 
 public final class InMemoryDexClassLoader extends DexClassLoader {
 
-    public InMemoryDexClassLoader(ByteBuffer[] buffers, ClassLoader parent) {
-        super("", null, null, parent);
+    public InMemoryDexClassLoader(
+            ByteBuffer[] buffers,
+            ClassLoader parent) {
+        this(createDexPath(buffers), parent);
+    }
+
+    public InMemoryDexClassLoader(
+            ByteBuffer buffer,
+            ClassLoader parent) {
+        this(new ByteBuffer[]{buffer}, parent);
+    }
+
+    private InMemoryDexClassLoader(
+            DexPath path,
+            ClassLoader parent) {
+        super(
+                path.dex.getAbsolutePath(),
+                path.optimized.getAbsolutePath(),
+                null,
+                parent
+        );
+    }
+
+    private static DexPath createDexPath(
+            ByteBuffer[] buffers) {
+
+        if (buffers == null || buffers.length == 0) {
+            throw new NullPointerException("buffers");
+        }
+
         try {
-            int totalSize = 0;
-            for (ByteBuffer buf : buffers) totalSize += buf.remaining();
-            byte[] dexBytes = new byte[totalSize];
-            int offset = 0;
-            for (ByteBuffer buf : buffers) {
-                int len = buf.remaining();
-                buf.get(dexBytes, offset, len);
-                offset += len;
+            File root = new File(
+                    System.getProperty("java.io.tmpdir"),
+                    "dex"
+            );
+
+            if (!root.exists() && !root.mkdirs()) {
+                throw new IOException(
+                        "Cannot create optimized directory"
+                );
             }
 
-            Class<?> dexFileClass = DexFile.class;
-            Method openDexFileMethod = dexFileClass.getDeclaredMethod("openDexFile", byte[].class);
-            openDexFileMethod.setAccessible(true);
-            Object cookie = openDexFileMethod.invoke(null, (Object) dexBytes);
+            File dex = File.createTempFile(
+                    "memory-",
+                    ".dex",
+                    root
+            );
 
-            Field pathListField = BaseDexClassLoader.class.getDeclaredField("pathList");
-            pathListField.setAccessible(true);
-            Object pathList = pathListField.get(this);
+            try (FileOutputStream out =
+                         new FileOutputStream(dex)) {
 
-            Field dexElementsField = pathList.getClass().getDeclaredField("dexElements");
-            dexElementsField.setAccessible(true);
-            Object[] dexElements = (Object[]) dexElementsField.get(pathList);
+                byte[] data = new byte[8192];
 
-            Field dexFileField = dexElements[0].getClass().getDeclaredField("dexFile");
-            dexFileField.setAccessible(true);
-            Object dexFileObj = dexFileField.get(dexElements[0]);
+                for (ByteBuffer buffer : buffers) {
+                    if (buffer == null) {
+                        throw new NullPointerException(
+                                "buffer"
+                        );
+                    }
 
-            Field mCookieField = dexFileClass.getDeclaredField("mCookie");
-            mCookieField.setAccessible(true);
-            mCookieField.set(dexFileObj, cookie);
-        } catch (Throwable ignored) {}
+                    ByteBuffer copy = buffer.slice();
+
+                    while (copy.hasRemaining()) {
+                        int length = Math.min(
+                                copy.remaining(),
+                                data.length
+                        );
+
+                        copy.get(data, 0, length);
+                        out.write(data, 0, length);
+                    }
+                }
+            }
+
+            File optimized = new File(
+                    root,
+                    dex.getName() + ".odex"
+            );
+
+            return new DexPath(
+                    dex,
+                    optimized
+            );
+
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public InMemoryDexClassLoader(ByteBuffer buffer, ClassLoader parent) {
-        this(new ByteBuffer[]{ buffer }, parent);
+    private static final class DexPath {
+        final File dex;
+        final File optimized;
+
+        DexPath(
+                File dex,
+                File optimized) {
+            this.dex = dex;
+            this.optimized = optimized;
+        }
     }
             }
-                                            
