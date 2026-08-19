1ac5d998c8b88de389a1626ff92e8db9bbebb074
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-19 12:31:47 +0700

    Update DexFile.java

--- oldlib/dalvik/system/DexFile.java
+++ oldlib/dalvik/system/DexFile.java
@@ -2,6 +2,7 @@ package oldlib.dalvik.system;
 
 import java.nio.ByteBuffer;
 import java.io.IOException;
+import java.util.List;
 
 public final class DexFile {
   private Object mCookie;
@@ -17,12 +18,22 @@ DexFile(ByteBuffer buf) throws IOException {
     mFileName = null;
 }
 
+public static DexFile loadDex(String path, String optimizedDirectory, int flags) throws IOException {
+    Object cookie = openDexFile(path, optimizedDirectory, flags);
+    return new DexFile(cookie);
+}
+
+private DexFile(Object cookie) {
+    this.mCookie = cookie;
+    this.mInternalCookie = cookie;
+    this.mFileName = null;
+}
+  
 private static Object openInMemoryDexFile(ByteBuffer buf)
         throws IOException {
     if (buf == null) {
         throw new NullPointerException("buf == null");
     }
-
     if (buf.isDirect()) {
         return createCookieWithDirectBuffer(
                 buf,
@@ -36,6 +47,17 @@ private static Object openInMemoryDexFile(ByteBuffer buf)
     }
 }
 
+public Class<?> loadClass(String name, ClassLoader loader) {
+    return loadClassBinaryName(name, loader, null);
+  }
+
+public Class<?> loadClassBinaryName(String str, ClassLoader classLoader, List<Throwable> suppressed) {
+    return defineClass(str, classLoader, this.mCookie, this, suppressed);
+  }
+  
 private static native Object createCookieWithDirectBuffer(ByteBuffer buf, int start, int end) throws IOException;
 private static native Object createCookieWithArray(byte[] buf, int start, int end) throws IOException;
+private static native Object openDexFile(String path, String optimizedDirectory, int flags) throws IOException;
+private static native Class<?> defineClass(String name, ClassLoader loader, Object cookie, DexFile dexFile, List<Throwable> suppressed);
+}
 } 
