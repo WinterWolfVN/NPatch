56e822318d1426dd1f930c4238283285df712cb7
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-19 14:48:24 +0700

    Update InMemoryDexClassLoader.java

--- oldlib/dalvik/system/InMemoryDexClassLoader.java
+++ oldlib/dalvik/system/InMemoryDexClassLoader.java
@@ -1,10 +1,24 @@
 package oldlib.dalvik.system;
 
+import android.os.Build;
 import java.nio.ByteBuffer;
 
 public final class InMemoryDexClassLoader
         extends BaseDexClassLoader {
-
+                
+    public static ClassLoader create(ByteBuffer buffer, ClassLoader parent) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            try {                
+                Class<?> systemClass = Class.forName("dalvik.system.InMemoryDexClassLoader");
+                Constructor<?> constructor = systemClass.getConstructor(ByteBuffer.class, ClassLoader.class);
+                return (ClassLoader) constructor.newInstance(buffer, parent);
+            } catch (Exception e) {
+                e.printStackTrace();                
+            }
+        }                
+        return new InMemoryDexClassLoader(buffer, parent);
+}
+                
     public InMemoryDexClassLoader(
             ByteBuffer[] dexBuffers,
             ClassLoader parent) {
