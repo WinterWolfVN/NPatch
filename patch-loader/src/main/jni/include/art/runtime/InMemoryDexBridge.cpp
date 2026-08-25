8abcdfba358c0b99cf9c8fbdcc56a9e21ad9509c
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-25 20:13:25 +0700

    Undertale

--- patch-loader/src/main/jni/include/art/runtime/InMemoryDexBridge.cpp
+++ patch-loader/src/main/jni/include/art/runtime/InMemoryDexBridge.cpp
@@ -35,20 +35,6 @@ OpenMemoryFn GetOpenMemory() {
     return fn;
 }
 
-bool RegisterInMemoryDexBridge(JNIEnv* env) {
-    if (env == nullptr) {
-        return false;
-    }
-    if (android_get_device_api_level() > 26) {
-        return true;
-    }
-    jclass clazz = env->FindClass("oldlib/dalvik/system/DexFile");
-    if (clazz == nullptr) {
-        return false;
-    }
-    return env->RegisterNatives(clazz, gMethods, sizeof(gMethods) / sizeof(gMethods[0])) == JNI_OK;
-}
-
 jobject CreateCookie(JNIEnv* env, const DexFile* dexFile) {
     jlongArray cookie = env->NewLongArray(2);
     if (cookie == nullptr) {
@@ -156,5 +142,19 @@ const JNINativeMethod gMethods[] = {
         "createCookieWithDirectBuffer", "(Ljava/nio/ByteBuffer;II)Ljava/lang/Object;", reinterpret_cast<void*>(CreateCookieWithDirectBuffer)
         }
 };
+
+bool RegisterInMemoryDexBridge(JNIEnv* env) {
+    if (env == nullptr) {
+        return false;
+    }
+    if (android_get_device_api_level() > 26) {
+        return true;
+    }
+    jclass clazz = env->FindClass("oldlib/dalvik/system/DexFile");
+    if (clazz == nullptr) {
+        return false;
+    }
+    return env->RegisterNatives(clazz, gMethods, sizeof(gMethods) / sizeof(gMethods[0])) == JNI_OK;
+}
 
 }
