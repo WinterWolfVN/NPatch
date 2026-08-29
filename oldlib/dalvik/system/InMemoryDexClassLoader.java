78cda326418dbfde43ffdf13564d1161ae8f0ad5
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-29 17:21:05 +0700

    Update InMemoryDexClassLoader.java

--- oldlib/dalvik/system/InMemoryDexClassLoader.java
+++ oldlib/dalvik/system/InMemoryDexClassLoader.java
@@ -1,20 +1,30 @@
 package oldlib.dalvik.system;
 
-import oldlib.dalvik.system.BaseDexClassLoader;
-import java.nio.ByteBuffer;
+import dalvik.system.DexFile;
+import java.lang.reflect.Method;
 
-public final class InMemoryDexClassLoader
-        extends BaseDexClassLoader {                  
-                
-    public InMemoryDexClassLoader(
-            ByteBuffer[] dexBuffers,
-            ClassLoader parent) {
-        super(dexBuffers, parent);
+public final class InMemoryDexClassLoader extends ClassLoader {
+
+    private final DexFile dexFile;
+
+    public InMemoryDexClassLoader(DexFile dexFile, ClassLoader parent) {
+        super(parent);
+        this.dexFile = dexFile;
     }
 
-    public InMemoryDexClassLoader(
-            ByteBuffer dexBuffer,
-            ClassLoader parent) {
-        this(new ByteBuffer[]{dexBuffer}, parent);
-    }
+    @Override
+    protected Class<?> findClass(String name)
+            throws ClassNotFoundException {
+        try {
+            Method method = DexFile.class.getDeclaredMethod("loadClassBinaryName", String.class, ClassLoader.class, java.util.List.class);
+            method.setAccessible(true);
+            Class<?> result = (Class<?>) method.invoke(dexFile, name.replace('.', '/'), this, null);
+            if (result != null) {
+                return result;
+            }
+        } catch (ReflectiveOperationException e) {
+            throw new ClassNotFoundException(name, e);
         }
+        throw new ClassNotFoundException(name);
+    }
+}
