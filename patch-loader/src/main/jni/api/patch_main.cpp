fa3de76518c8030bfc09f6083246f8554ee071b2
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-30 14:19:59 +0700

    Final

--- patch-loader/src/main/jni/api/patch_main.cpp
+++ patch-loader/src/main/jni/api/patch_main.cpp
@@ -27,6 +27,7 @@
 #include "patch_loader.h"
 #include <android/api-level.h>
 
+bool RegisterInMemoryDexClassLoader(JNIEnv* env);
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
     JNIEnv* env;
     if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
